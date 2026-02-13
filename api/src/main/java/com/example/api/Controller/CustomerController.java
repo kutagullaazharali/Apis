549fa959b74cc24a3c766d7a9893e555ade2ce1e
Azhar Ali <azharali87901@gmail.com>
2026-02-13 23:56:28 +0530

    Rate Limit

--- api/src/main/java/com/example/api/Controller/CustomerController.java
+++ api/src/main/java/com/example/api/Controller/CustomerController.java
@@ -1,14 +1,21 @@
 package com.example.api.Controller;
-import com.example.api.Model.Customer;
-import com.example.api.Services.CustomerService;
-import lombok.RequiredArgsConstructor;
-import java.util.List; 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.api.Model.Customer;
+import com.example.api.Services.CustomerService;
+
+import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(origins = "http://localhost:3000")
 
 @RestController
 @RequestMapping("/api/customers")
