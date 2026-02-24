070b08031d9e58755dab47f3f71db40a241380f2
Azhar Ali <azharali87901@gmail.com>
2026-02-25 00:17:29 +0530

    added crossorigin

--- api/src/main/java/com/example/api/Controller/CustomerController.java
+++ api/src/main/java/com/example/api/Controller/CustomerController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +17,7 @@ import com.example.api.Services.CustomerService;
 
 import lombok.RequiredArgsConstructor;
 
-//@CrossOrigin(origins = "http://localhost:3000")
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/customers")
 @RequiredArgsConstructor
